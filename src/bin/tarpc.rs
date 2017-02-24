b4dcb6644d3beb8da043f9587ac61ed83451b35e
Jon Gjengset <jon@thesquareplanet.com>
2017-02-24 16:16:26 -0500

    clean up and make similar

---
+++ src/bin/tarpc.rs
@@ -0,0 +1,59 @@
+#![feature(plugin)]
+#![plugin(tarpc_plugins)]
+
+#[macro_use]
+extern crate tarpc_bench;
+
+#[macro_use]
+extern crate tarpc;
+
+extern crate futures;
+extern crate tokio_core;
+
+use tarpc::client;
+use tarpc::server;
+use tarpc::util::Never;
+use tarpc::util::FirstSocketAddr;
+use tokio_core::reactor;
+
+mod srv {
+    service! {
+        rpc test(i: u64) -> u64;
+    }
+}
+
+#[derive(Clone)]
+struct Srv;
+impl srv::FutureService for Srv {
+    type TestFut = futures::Finished<u64, Never>;
+    fn test(&self, i: u64) -> Self::TestFut {
+        futures::finished(i * 2)
+    }
+}
+
+
+fn main() {
+    use std::time;
+    let n = 100000;
+
+    use srv::FutureServiceExt;
+    let mut reactor = reactor::Core::new().unwrap();
+    let (addr, server) = Srv.listen("127.0.0.1:2223".first_socket_addr(),
+                &reactor.handle(),
+                server::Options::default())
+        .unwrap();
+    reactor.handle().spawn(server);
+
+    use client::future::ClientExt;
+    let client = srv::FutureClient::connect(addr,
+                                            client::Options::default().handle(reactor.handle()));
+    let client = reactor.run(client).unwrap();
+
+
+    let start = time::Instant::now();
+    for i in 0..n {
+        reactor.run(client.test(i)).unwrap();
+    }
+    println!("tarpc {:.0}µs/call",
+             dur_to_ns!(start.elapsed()) as f64 / n as f64 / 1000.0);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,3 @@
-#![feature(plugin)]
-#![plugin(tarpc_plugins)]
-
-#[macro_use]
-extern crate serde_derive;
-
-#[macro_use]
-extern crate tarpc;
-
-extern crate futures;
-extern crate tokio_core;
-extern crate serde;
-
-extern crate bincode;
-
-use std::sync;
-
-
 #[macro_export]
 macro_rules! dur_to_ns {
     ($d:expr) => {{
@@ -23,18 +5,4 @@ macro_rules! dur_to_ns {
         let d = $d;
         d.as_secs() * NANOS_PER_SEC + d.subsec_nanos() as u64
     }}
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum DataType {
-    None,
-    Number(i64),
-    Text(sync::Arc<String>),
-}
-
-pub mod no {
-    use super::serde;
-    service! {
-        rpc test(i: serde::bytes::ByteBuf) -> serde::bytes::ByteBuf;
-    }
 }
